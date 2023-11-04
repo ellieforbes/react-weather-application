62fb24fff386b32c0a7bec55228941d234bb0a13
ellieforbes <139644804+ellieforbes@users.noreply.github.com>
2023-11-04 20:37:48 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-       Weather App
+      <h1> Weather App</h1>
       </header>
       <footer>
         This project was coded by {" "}
         <a href="https://portfolio-byellieforbes.netlify.app/" target="_blank" rel="noreferrer" >Ellie Forbes</a> and is {" "}
         <a href="https://github.com/ellieforbes/react-weather-application" target="_blank" rel="noreferrer" >open-sourced on Github</a>
       </footer>
+      </div>
     </div>
   );
 }
