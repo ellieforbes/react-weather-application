7e03f8b708b4577e42f66a9f219ff2c8f1e2a6fb
ellieforbes <139644804+ellieforbes@users.noreply.github.com>
2023-11-04 19:29:20 +0000

    Added link to github repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header className="App-header">
        Weather App
       </header>
+      <footer>
+        This project was coded by {" "}
+        <a href="https://portfolio-byellieforbes.netlify.app/" target="_blank" rel="noreferrer" >Ellie Forbes</a> and is {" "}
+        <a href="https://github.com/ellieforbes/react-weather-application" target="_blank" rel="noreferrer" >open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
